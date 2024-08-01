e22ca995dd9f575fc89338dad0e64cae42254b5a
zhanglab-wbgcas <zhangxj@wbgcas.cn>
2024-08-01 11:30:16 +0800

    Update model.py

--- model/model.py
+++ model/model.py
@@ -2,19 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import warnings
-warnings.filterwarnings('ignore')  # Ignore warnings
+warnings.filterwarnings('ignore')
 import math
 import pandas as pd
-from tqdm.auto import tqdm  # tqdm for progress bars
-from torch.utils.data import DataLoader, Dataset  # DataLoader and Dataset for handling data
+from tqdm.auto import tqdm
+from torch.utils.data import DataLoader, Dataset
 import numpy as np
 import random
-import torch.optim.lr_scheduler as lr_scheduler  # Learning rate scheduler
-from torch.utils.data import DataLoader  # DataLoader for loading data
-from retnet import RetNet  # Import RetNet module
-from sklearn.metrics import mean_squared_error  # Import mean squared error metric
+import torch.optim.lr_scheduler as lr_scheduler 
+from torch.utils.data import DataLoader
+from retnet import RetNet
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import os
 
-# Check if GPU is available, otherwise use CPU
+# Ensure CUDA launch blocking
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+
+# Check if CUDA is available and set the device accordingly
 if torch.cuda.is_available():
     device = torch.device("cuda")
     print(f"GPU ({torch.cuda.get_device_name(0)}) is available and will be used.")
@@ -22,7 +26,7 @@ else:
     device = torch.device("cpu")
     print("GPU is not available; falling back to CPU.")
 
-# Function to set random seeds for reproducibility
+# Function to set the same seed for reproducibility
 def same_seeds(seed):
     random.seed(seed)
     np.random.seed(seed)
@@ -33,19 +37,16 @@ def same_seeds(seed):
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
-# Set random seeds
-same_seeds(2023)
-
-# Custom EarlyStopping class to handle early stopping during training
+# EarlyStopping class to handle early stopping during training
 class EarlyStopping:
     def __init__(self, patience=10, verbose=False, delta=0):
-        self.patience = patience
-        self.verbose = verbose
-        self.counter = 0
-        self.best_score = -np.Inf  # Initialize with a negative value
-        self.early_stop = False
-        self.val_loss_min = np.Inf
-        self.delta = delta
+        self.patience = patience  
+        self.verbose = verbose  
+        self.counter = 0  
+        self.best_score = -np.Inf  
+        self.early_stop = False  
+        self.val_loss_min = np.Inf  
+        self.delta = delta  
 
     def early_stopping(self, val_score):
         if val_score > self.best_score + self.delta:
@@ -57,30 +58,33 @@ class EarlyStopping:
         if self.counter >= self.patience:
             self.early_stop = True
 
-# Function to split data into sub-vectors and convert to PyTorch tensors
-def getXY(gap, snp, phenotype):
+# Function to preprocess SNP and phenotype data and get features and labels
+def getXY(windows, snp, phenotype):
     snp_list = []
     for i in range(len(snp)):
         sample = snp.iloc[i, :]
         feature = []
-        length = len(sample)
-        # Splitting the vector into sub-vectors of length 'gap'
-        for k in range(0, length, gap):
-            if (k + gap <= length):
-                a = sample[k:k + gap]
+        or_length = len(sample)
+        if windows % 2 == 1:
+            windows = windows + 1
+        padded_sample = np.pad(sample, (0, windows - or_length if or_length < windows else 0), mode='constant')
+        length = len(padded_sample)
+        for k in range(0, length, windows):
+            if (k + windows <= length):
+                a = padded_sample[k:k + windows]
             else:
-                a = sample[length - gap:length]
+                a = padded_sample[length - windows:length]
             feature.append(a)
         feature = np.asarray(feature, dtype=np.float32)
         snp_list.append(feature)
-    snp_list = np.asarray(snp_list)
-    
+
+    snp_list = np.asarray(snp_list)  # (n_samples, gap_num, gap)
     y_train = phenotype.values  
     y_train = y_train.astype(np.float32)
     y_train = pd.DataFrame(y_train)
     return snp_list, y_train
 
-# Custom dataset class for handling data in DataLoader
+# Custom dataset class to handle data and labels
 class DataSet(Dataset):
     def __init__(self, data, label):
         self.data = data
@@ -94,61 +98,63 @@ class DataSet(Dataset):
         label = torch.tensor(self.label.iloc[index], dtype=torch.float32)
         return data, label
 
-# Custom plantGPT model class inheriting from nn.Module
+
 class plantGPT(nn.Module):
-    def __init__(self, input_dim, hidden_size, ffn_size, nhead=4, d_model=64):
+    def __init__(self, input_dim, layers, hidden_size, ffn_size, nhead=4, d_model=64):
         super().__init__()
-        # Initialize RetNet module
-        self.retnet = RetNet(hidden_size, ffn_size, heads=nhead, double_v_dim=True)
-        # Define prediction layers
-        self.pred = nn.Sequential(
-            nn.Linear(d_model, d_model),
-            nn.GELU(),
-            nn.LayerNorm(d_model),
-            nn.Linear(d_model, d_model//4),
-            nn.GELU(),
-            nn.LayerNorm(d_model//4),
-            nn.Linear(d_model//4, d_model//8),
-            nn.GELU(),
-            nn.Linear(d_model//8, 1)
-        )
-        
+        self.retnet = RetNet(layers, hidden_size, ffn_size, heads=nhead, double_v_dim=True)
+        if d_model > 200:
+            self.pred = nn.Sequential(
+                nn.Linear(d_model, d_model),
+                nn.GELU(),
+                nn.Linear(d_model, d_model // 2),
+                nn.GELU(),
+                nn.Linear(d_model // 2, d_model // 4),
+                nn.GELU(),
+                nn.Linear(d_model // 4, 1),
+            )
+        else:
+            self.pred = nn.Sequential(
+                nn.Linear(d_model, 128),
+                nn.Linear(128, 1)
+            )
+
     def forward(self, mels):
-        # Permute input tensor
         x = mels.permute(1, 0, 2)
-        # Pass through RetNet module
         x = self.retnet(x)
         x = x.transpose(0, 1)
         # Mean pooling
         x = x.mean(dim=1)
-        # Pass through prediction layers
         x = self.pred(x)
         return x
-def train_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_train, y_train, x_val, y_val, lr, model_path):
-    """
-    Train the plantGPT model.
 
-    Args:
-        gap (int): Gap size.
-        hidden_size (int): Hidden size for the model.
-        ffn_size (int): Size of the feedforward neural network.
-        heads (int): Number of attention heads.
-        batch_sizes (int): Batch size for training.
-        x_train (DataFrame): Training input data.
-        y_train (DataFrame): Training target data.
-        x_val (DataFrame): Validation input data.
-        y_val (DataFrame): Validation target data.
-        lr (float): Learning rate.
-        model_path (str): Path to save the trained model.
+# Function to train the model
+def train_model(windows, d_model, layers, hidden_size, ffn_size, heads, batch_sizes, x_train, y_train, x_val, y_val, lr, model_path):
     """
-    d_models = gap
+    Parameters:
+    - windows: Window size for segmenting the SNP data.
+    - d_model: Dimension of the model.
+    - layers: Number of layers in the RetNet.
+    - hidden_size: Hidden size in each layer of the RetNet.
+    - ffn_size: Feedforward network size in each layer of the RetNet.
+    - heads: Number of attention heads in the RetNet.
+    - batch_sizes: Batch size for training and validation.
+    - x_train: Training features.
+    - y_train: Training labels.
+    - x_val: Validation features.
+    - y_val: Validation labels.
+    - lr: Learning rate for the optimizer.
+    - model_path: Path to save the best model.
+    """
+    d_model = d_model
     n_epochs = 25
-    x_train, y_train = getXY(gap, x_train, y_train)
-    x_val, y_val = getXY(gap, x_val, y_val)
+
+    x_train, y_train = getXY(windows, x_train, y_train)
+    x_val, y_val = getXY(windows, x_val, y_val)
 
     early_stopping = EarlyStopping(patience=10, verbose=True, delta=0.001)
     
-    model = plantGPT(input_dim=d_models, hidden_size=hidden_size, ffn_size=ffn_size, nhead=heads, d_model=d_models)
+    model = plantGPT(input_dim=d_model, layers=layers, hidden_size=hidden_size, ffn_size=ffn_size, nhead=heads, d_model=d_model)
     model.to(device)
 
     # Setting loss function
@@ -156,7 +162,7 @@ def train_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_train, y_train
     # Setting optimization function
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
     # Creating the ReduceLROnPlateau scheduler
-    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=6, verbose=True)
+    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=5, verbose=True)
 
     # Setting the training dataset
     train_dataset = DataSet(data=x_train, label=y_train)
@@ -168,9 +174,8 @@ def train_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_train, y_train
 
     best_coe = -float('inf') 
     best_model_state = None
-    y_val = y_val.iloc[:, 0]  # Extracting '0' column values and converting to a one-dimensional array
+    y_val = y_val.iloc[:, 0]  
     for epoch in range(n_epochs):
-       
         model.train()
         train_loss = []
         
@@ -185,11 +190,9 @@ def train_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_train, y_train
             train_loss.append(loss.item())
             
         train_loss = sum(train_loss) / len(train_loss)
-            
         print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}")
         
         model.eval()
-        
         preds = []
         with torch.no_grad():  # Disable gradient calculation
             for batch in tqdm(val_loader):
@@ -198,17 +201,16 @@ def train_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_train, y_train
                 batch_preds = model(data)
                 preds.extend(batch_preds.cpu().numpy())
         preds = np.concatenate(preds, axis=0) 
-          
         mse = mean_squared_error(y_val, preds)
         coe = np.corrcoef(y_val, preds)[0, 1]
-
+        mae = mean_absolute_error(y_val, preds)
         print(f"Val mse = {mse:.4f} ")
+        print(f"Val mae = {mae:.4f} ")
         print(f"Val coe = {coe:.4f} ")
-
         if coe > best_coe:
             best_coe = coe
             best_model_state = model.state_dict().copy() 
-        early_stopping.early_stopping(coe)  # Pass validation set's coe score
+        early_stopping.early_stopping(coe)  # Pass the coe score on the validation set
 
         scheduler.step(coe)
         if early_stopping.early_stop:
@@ -222,27 +224,20 @@ def train_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_train, y_train
         }
         torch.save(save_dict, model_path)
 
-def pre_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_test, y_test, model_path, output_path_coe, output_path_mse):
+# Function to predict using the trained model
+def pre_model(windows, d_model, layers, hidden_size, ffn_size, heads, batch_sizes, x_test, y_test, model_path, output_path_coe=None, output_path_mse=None):
     """
-    Predict using the trained plantGPT model.
-
-    Args:
-        gap (int): Gap size.
-        hidden_size (int): Hidden size for the model.
-        ffn_size (int): Size of the feedforward neural network.
-        heads (int): Number of attention heads.
-        batch_sizes (int): Batch size for prediction.
-        x_test (DataFrame): Test input data.
-        y_test (DataFrame): Test target data.
-        model_path (str): Path to the trained model.
-        output_path_coe (str): Path to save the coefficient of correlation (coe).
-        output_path_mse (str): Path to save the mean squared error (mse).
+    Parameters:
+    - x_test: Test features.
+    - y_test: Test labels.
+    - model_path: Path to load the trained model.
+    - output_path_coe: Path to save the coefficient results.
+    - output_path_mse: Path to save the MSE results.
     """
-    d_models = gap
-               
-    x_test, y_test = getXY(gap, x_test, y_test)
+    d_model = d_model    
+    x_test, y_test = getXY(windows, x_test, y_test)
     
-    model = plantGPT(input_dim=d_models, hidden_size=hidden_size, ffn_size=ffn_size, nhead=heads, d_model=d_models)
+    model = plantGPT(input_dim=d_model, layers=layers, hidden_size=hidden_size, ffn_size=ffn_size, nhead=heads, d_model=d_model)
     model.to(device)
     saved_state_dict = torch.load(model_path)
     model.load_state_dict(saved_state_dict['model'])
@@ -251,7 +246,6 @@ def pre_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_test, y_test, mo
     test_loader = DataLoader(test_dataset, batch_size=batch_sizes, shuffle=False, pin_memory=True)
 
     model.eval()
-
     preds = []
     with torch.no_grad():  # Disable gradient calculation
         for batch in tqdm(test_loader):
@@ -260,11 +254,12 @@ def pre_model(gap, hidden_size, ffn_size, heads, batch_sizes, x_test, y_test, mo
             batch_preds = model(data)
             preds.extend(batch_preds.cpu().numpy())
     preds = np.concatenate(preds, axis=0)
-    y_test = y_test.iloc[:, 0]  # Extract column values and convert to a one-dimensional array
+    y_test = y_test.iloc[:, 0]  
     coe = np.corrcoef(y_test, preds)[0, 1]
     mse = mean_squared_error(y_test, preds)
-     
-    with open(output_path_coe, 'a') as f:
-        f.write(f"{coe}\n")
-    with open(output_path_mse, 'a') as f:
-        f.write(f"{mse}\n") 
+    print(f"Test coe = {coe:.6f} ")
+    print(f"Test mse = {mse:.6f} ")
+    #with open(output_path_coe, 'a') as f:
+        #f.write(f"{coe}\n")
+    #with open(output_path_mse, 'a') as f:
+        #f.write(f"{mse}\n")
